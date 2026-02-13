84c2fa94f1f266e4715bf4c0b08574b16a88272f
MJB9595 <wansja7870@gmail.com>
2026-02-13 11:26:28 +0900

    useContext으로 수정 및 구조 개편

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
-import "./style/TodoItem.css"
+import "./TodoItem.css"
+import { useTodoStore } from '../contexts/TodoContext'
 const TodoItem = ({
   id,
   isDone,
   content,
   date,
-  onUpdate,
-  onDelete}) => {
+}) => {
 
-    const onChangeCheckbox =()=> {
-      onUpdate(id)
-    }
+  const {toggleTodo, deleteTodo}= useTodoStore()
 
-    const onDeleteConfirm =()=> {
-      onDelete(id)   
-    }
+
   return (
-    <div className={`TodoItem ${isDone? 'done':''}`}>
-        <input type="checkbox"  checked={isDone} onChange={onChangeCheckbox}/>
-        <div className="content">{content}</div>
-        <div className="date">{new Date(date).toLocaleDateString()}</div>
-        <button onClick={onDeleteConfirm}>삭제</button>
+    <div className={`TodoItem ${isDone ? 'done' : ''}`}>
+      <input type="checkbox" 
+      checked={isDone} 
+      onChange={()=>toggleTodo(id)} />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={()=>deleteTodo(id)}>삭제</button>
     </div>
   )
 }
